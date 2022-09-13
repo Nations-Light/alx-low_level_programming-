4eabe932cee753bdcaefddecba5d4efdf7c6f42d
Nations-Light <queenjoseph661@gmail.com>
2022-09-13 02:45:19 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,99 +1,50 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <time.h>
 /**
-
-
  * main - print possible combination of two 2-digit
-
-
  * numbers
-
-
  *
-
-
  * Return: returns zero at the end
-
-
  */
-
-
 int main(void)
+{
 
+int i, p;;
+
+for (i = 0; i < 100; i++)
 
 {
 
+for (p = 0; p < 100; j++)
 
-    int i, p;
+{
+	
+if (p > i)
 
+{
+                
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+                
+if (i != 98)
 
+continue;
+                
+{
+                    
+putchar(',');
+putchar(' ');
+                
+}
 
+}
 
+putchar('\n');
 
-
-
-    for (i = 0; i < 100; i++)
-
-
-    {
-
-
-        for (p = 0; p < 100; j++)
-
-
-        {
-
-
-            if (p > i)
-
-
-            {
-
-
-                putchar((i / 10) + '0');
-
-
-                putchar((i % 10) + '0');
-
-
-                putchar(' ');
-
-
-                putchar((p / 10) + '0');
-
-
-                putchar((p % 10) + '0');
-
-
-                if (i != 98)
-
-
-                {
-
-
-                    putchar(',');
-
-
-                    putchar(' ');
-
-
-                }
-
-
-            }
-
-
-        }
-
-
-    }
-
-
-    putchar('\n');
-
-
-    return (0);
-
+return (0);
 
 }
