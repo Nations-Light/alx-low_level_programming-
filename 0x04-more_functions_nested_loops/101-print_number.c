38c1e08f4db9a4ec14a8742d17def291ab1d6e84
Nations-Light <queenjoseph661@gmail.com>
2022-09-15 13:27:34 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * print_number - Prints a number
 * @n: The number to print
 */
-
 void print_number(int n)
 {
 	unsigned int num = n;
-
 	if (n < 0)
 	{
 		putchar('-');
