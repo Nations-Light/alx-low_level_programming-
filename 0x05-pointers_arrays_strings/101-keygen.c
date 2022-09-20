2bfcb908385e93421c1437c72910948c8bb736a0
Nations-Light <queenjoseph661@gmail.com>
2022-09-19 18:22:30 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - entry point
- *
- * Return: generated password
  */
-
 int main(void)
 {
-	char c;
-	int x;
+int num;
 
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
-
-	return (0);
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
 }
