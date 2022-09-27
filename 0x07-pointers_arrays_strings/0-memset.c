895817767110d206d910ec6c2628e1cb66bdfa5b
Nations-Light <queenjoseph661@gmail.com>
2022-09-26 20:23:51 -0700

    new 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,49 @@
-#include "main.h
+#include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - fill a block of memory
- * @s: starting address
- * @b: value
- * @n: no of bytes
- * Return: Always 0
- */
-char *_memset(char *s, char b, unsigned int n);
-}
-	int i = 0
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+*/
 
-	for (; n > 0; i++)
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
 	{
-		s[i] = b;
-		n--;
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
 	}
-	return (s);
+	printf("\n");
+}
+
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
